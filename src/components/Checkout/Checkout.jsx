3ad21d6c507a86d2e18d5a81c885f3c99cbd238f
Chris Mosier <chrismosier13@gmail.com>
2017-12-08 11:45:23 -0800

    set up pages and components

---
+++ src/components/Checkout/Checkout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Checkout = () => {
+    return(
+        <div>
+            page where you buy our shit on!
+        </div>
+    )
+}
+
+export default Checkout
---
+++ src/components/Confirmation/Confirmation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Confirmation = () => {
+    return (
+        <div>
+            confirmed you bought my shit
+        </div>
+    )
+}
+
+export default Confirmation;
